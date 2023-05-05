246577d64ed3b675e68ef45b06d64c3bb24254f5
chosotto09@gmail.com <chosotto09@gmail.com>
2023-05-06 00:30:33 +0600

    day4

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        progressBar=findViewById(R.id.progress_circular);
+
+
+        new Handler().postDelayed(() -> {
+
+            startActivity(new Intent(MainActivity.this,DeshbordActivity.class));
+
+        },1500);
     }
 }
