37ca05ce7cedf551c12d1c8431960992e3619376
chosotto09@gmail.com <chosotto09@gmail.com>
2023-05-06 02:07:15 +0600

    day4

--- app/src/main/java/com/example/quizapp/DeshbordActivity.java
+++ app/src/main/java/com/example/quizapp/DeshbordActivity.java
@@ -1,14 +1,42 @@
 package com.example.quizapp;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.widget.ProgressBar;
 
 public class DeshbordActivity extends AppCompatActivity {
 
+
+    CountDownTimer countDownTimer;
+    ProgressBar progressBar;
+    int timer=20;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deshbord);
+
+        progressBar=findViewById(R.id.progressBar);
+        countDownTimer=new CountDownTimer(20000,1000) {
+            @Override
+            public void onTick(long l) {
+                timer=timer-1;
+                progressBar.setProgress(timer);
+            }
+
+            @Override
+            public void onFinish() {
+                Log.d(TAG, "onFinish: done");
+                Dialog dialog=new Dialog(DeshbordActivity.this,R.style.dialog);
+                dialog.setContentView(R.layout.timer_dialog);
+                dialog.show();
+
+            }
+        }.start();
     }
 }
