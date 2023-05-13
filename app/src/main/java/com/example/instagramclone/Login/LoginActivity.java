036f6e03cfdcfbb97ef858198656015352874eec
chosotto09@gmail.com <chosotto09@gmail.com>
2023-05-13 23:27:17 +0600

    day4

--- app/src/main/java/com/example/instagramclone/Login/LoginActivity.java
+++ app/src/main/java/com/example/instagramclone/Login/LoginActivity.java
@@ -1,13 +1,16 @@
-package com.example.instagramclone;
+package com.example.instagramclone.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.instagramclone.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
